155a832e5e46bcb12023c4d07006045fe62af57e
Sharod Bailey <Sharod.Bailey@codeimmersives.com>
2020-04-14 15:12:21 -0400

    not done

--- main.js
+++ main.js
@@ -1,4 +1,46 @@
+function Node(value) {
+  return {
+    value: value,//this is the info
+    next: null,// this is the link
+  }
+}//link list are made up of nodes.... this is node 1
 
+function Lil() {
+  return {
+    head: null,
+
+    
+
+    addToStart: function(value){
+      let newNode = Node(value);
+      if(this.head === null){
+        this.head = newNode; //pointing at same node.
+      }else if (this.head) { 
+        newNode.next = this.head; //adds a new Node with the given value as the new head
+        this.head = newNode; //makes the new Node's next property the old head
+      }
+
+      
+      
+
+      
+//put value 
+
+    },
+
+    addToEnd: function(value){
+      let newNode = Node(value);
+      if(this.head === null){
+      this.head = newNode; 
+    } else if(this.head){
+      this.head = newNode.next;
+
+    }
+  },
+
+  }
+
+}
 
 
 
